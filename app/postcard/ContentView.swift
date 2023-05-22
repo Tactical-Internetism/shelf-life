0e441cb4bc7486cbeb0438c4b62a608bdc093a42
Jacob Wolf <jacob.h.wolf@gmail.com>
2023-05-21 23:42:19 -0400

    add qr code functionality

--- app/postcard/ContentView.swift
+++ app/postcard/ContentView.swift
@@ -6,13 +6,19 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @Binding var thisFridge: Fridge?
     @EnvironmentObject var dataManager: DataManager
     @State private var fridgeNumber: String = ""
     @State private var fridgeName: String = ""
-
+    @State private var showQR: Bool = false
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    
     var login: some View {
         VStack() {
             Image(systemName: "globe")
@@ -29,7 +35,7 @@ struct ContentView: View {
         }
         .padding()
     }
-
+    
     var chooseName: some View {
         VStack() {
             Image(systemName: "globe")
@@ -46,15 +52,36 @@ struct ContentView: View {
         }
         .padding()
     }
-
+    
+    var QRModal: some View {
+        VStack() {
+            Button {
+                showQR = false
+            } label: {
+                Text("Hide QR Code")
+            }
+            Image(uiImage: generateQRCode(from: "SMSTO:+\(thisFridge!.fridgeNumber):JOIN \(thisFridge!.fridgeName) on shelf life!"))
+                .resizable()
+                .interpolation(.none)
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+        }
+        .padding()
+    }
+    
     var shelf: some View {
         VStack() {
+            Button {
+                showQR = true
+            } label: {
+                Text("Show QR Code")
+            }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, \(thisFridge!.fridgeName)!")
             Button("Refresh", action: dataManager.fetchNewMessages)
-
+            
             ScrollView(.horizontal) {
                 HStack() {
                     ForEach(dataManager.messages, id: \.id) { message in
@@ -70,28 +97,47 @@ struct ContentView: View {
                     }
                 }
             }
-//                if dataManager.messages.count > 0 {
-//                    let message = dataManager.messages[0]
-//                    WebView(url: URL(string: message.link)!)
-//                        .border(.black)
-//                }
+            //                if dataManager.messages.count > 0 {
+            //                    let message = dataManager.messages[0]
+            //                    WebView(url: URL(string: message.link)!)
+            //                        .border(.black)
+            //                }
         }
         .padding()
         .onAppear {
             dataManager.fetchNewMessages()
         }
     }
-
+    
     var body: some View {
         if thisFridge == nil {
             login
         } else if thisFridge!.fridgeName.isEmpty {
             chooseName
+        } else if showQR {
+            QRModal
         } else {
             shelf
         }
     }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+//        filter.setValue(CGColor(red: 3, green: 3, blue: 3, alpha: 3), forKey: "kCIAttributeOpaqueColor")
+
+//        filter.kCIAttributeOpaqueColor = CGColor(red: 3, green: 3, blue: 3, alpha: 3)
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     @StateObject static private var dataManager = DataManager()
